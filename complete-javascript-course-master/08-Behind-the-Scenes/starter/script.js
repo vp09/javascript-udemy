d7a2b20121dba9c87ec2d3482417ee8f74371d69
Vidisha Pandey <vidisha09pandey@gmail.com>
2023-08-17 12:58:05 +0530

    initial files

---
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function calcAge(birthyear) {
+  let age = 2037 - birthyear;
+  console.log(firstName);
+
+  console.log(firstName1);
+  function check() {
+    age = 'check';
+  }
+  check();
+  console.log(age);
+}
+
+let firstName = 'jonas';
+calcAge(1991);
+var firstName1;
+console.log();
